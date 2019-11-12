66ee5c66ce1aeabf98413a200800fd06585a985f
Liam McClelland <mcclelland-109773@3aaa.ac.uk>
2019-11-12 18:44:02 +0000

    DEV 0.2.0
    Management area starting to come together. List below summaries changes
    - New auth route that controls authentication for areas.
    - New manage route that will handle all the management side of the system
    - Using templating engine to manage all the page html. Quite a few of these for the dashboard and screens are already in place.

---
+++ routes/auth.php
@@ -0,0 +1,81 @@
+<?php
+
+$app->group("/auth", function(){
+    
+    $this->get("/account", function($request, $response, $args) {
+        
+        $user = $this->get("user");
+        
+        if($user->loggedIn()) {
+        
+            print "<pre>"; print_r($_SESSION["user"]); print "</pre>";
+            exit;
+        
+        } else {
+            
+            die("NOT LOGGED IN");
+            
+        }
+        
+    });
+    
+    $this->post("/login", function($request, $response, $args) {
+        
+        $user = $this->get("user");
+        
+        $body = $request->getParsedBody();
+        
+        $required = array("username", "password");
+        
+        foreach($required as $item) {
+            
+            if(!isset($body[$item])) {
+                
+                die("MISSING INFO - $item");    
+                
+            }
+            
+        }
+        
+        $login = $user->login($body["username"], $body["password"]);
+        
+        if(!$login["status"]) {
+            
+            notifications::add("danger", $login["reason_desc"]);
+            
+            header("Location: https://" . $_SERVER["HTTP_HOST"] ."/Manage/authenticate/login");
+            exit;
+            
+        }
+                                    
+        header("Location: /Manage");
+        exit;
+        
+    });
+    
+    $this->get("/logout", function($request, $response, $args) {
+        
+        $user = $this->get("user");
+        
+        $user->logout("/Manage");
+        
+    });
+    
+    /*$this->get("/register/gfcAuth", function($request, $response, $args) {
+       
+       $user = $this->get("user");
+       
+       $reg = $user->register(array(
+            "user_id" => "admin",
+            "user_pwd" => "gfc123",
+            "user_name" => "Administrator",
+            "user_type" => "superuser",
+            "user_email" => "gfc@gadgetfreak.co.uk"
+       ));
+       
+       print "<pre>"; print_r($reg); print "</pre>";
+       exit;
+        
+    });*/
+    
+});
--- routes/manage.php
+++ routes/manage.php
@@ -4,14 +4,45 @@ $app->group("/Manage", function(){
 
     $this->get("", function($request, $response, $args) {
         
-        if(!isset($_SESSION["_manage"]["user"])) {
+        $user = $this->get("user");
+
+        if(!$user->loggedIn()) {
+
+            notifications::add("info", "Please login to access this page.");
             
             header("Location: /Manage/authenticate/login");
             exit;
             
         }
 
-        return $response = $this->manageView->render($response, "/dashboard/dashboard.phtml", ["_title" => "Manage"]);
+        return $response = $this->manageView->render($response, "/dashboard/dashboard.phtml", [
+            "_title" => "Manage",
+            "_user" => $_SESSION["user"],
+            "_page" => "dashboard"
+            ]);
+
+    });
+
+    /** SCREENS */
+
+    $this->get("/screens", function($request, $response, $args){
+
+        return $this->manageView->render($response, "/screens/overview.phtml", [
+            "_title" => "Manage Screens",
+            "_user" => $_SESSION["user"],
+            "_page" => "screens"
+        ]);
+
+
+    });
+
+    $this->get("/screens/new", function($request, $response, $args) {
+
+        return $this->manageView->render($response, "/screens/new.phtml", [
+            "_title" => "New Screen",
+            "_user" => $_SESSION["user"],
+            "_page" => "screens"
+        ]);
 
     });
 
@@ -19,12 +50,21 @@ $app->group("/Manage", function(){
     
     $this->get("/authenticate/login", function($request, $response, $args){
 
-        $_SESSION["_manage"]["user"] = true;
+        $user = $this->get("user");
+
+        if($user->loggedIn()) {
+
+            $user->redirect("/Manage");
+
+        }
 
         $html = file_get_contents("../templates/Manage/authentication/login.phtml");
         
-        return str_replace("<?=\$title?>", "Login | " . $_SESSION["system"]["name"], $html);
+        $errors = notifications::display() . "<br/>";
+
+        return str_replace(array("<?=\$title?>", "#errors#"), array("Login | " . $_SESSION["system"]["name"], $errors), $html);
         
     });
+
 
 });
