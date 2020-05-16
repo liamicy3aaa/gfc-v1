a3880e885f3fbc8023dd38363809ae8d5f4576b2
Liam McClelland <mcclelland-109773@3aaa.ac.uk>
2020-05-16 21:27:01 +0100

    DEV 1.3
    New Booking Wizard
    I have built a script that allows us to run a multi-step wizard easily. We simply provide a step name, onload function, onback function and onnext function for each step in the process.
    
    The onLoad function is called when the step is loaded. the onback and OnNext functions are called when the user either navigates forward or back allowing us to intercept this and ensure we can run code to save or revert changes made on the current step.
    
    If the user tries to navigate forward and we don't want them to due to not providing enough or valid information, we can return false in our onNext function which will stop the navigaton continuing and we can handle the error or alert asking for more information.
    
    I will now be starting work on making use of this tool with our main booking page.

---
+++ public/assets/js/Stepper.js
@@ -0,0 +1,411 @@
+function mergeDeep(...objects) {
+    const isObject = obj => obj && typeof obj === 'object';
+
+    return objects.reduce((prev, obj) => {
+        Object.keys(obj).forEach(key => {
+            const pVal = prev[key];
+            const oVal = obj[key];
+
+            if (Array.isArray(pVal) && Array.isArray(oVal)) {
+                prev[key] = pVal.concat(...oVal);
+            }
+            else if (isObject(pVal) && isObject(oVal)) {
+                prev[key] = mergeDeep(pVal, oVal);
+            }
+            else {
+                prev[key] = oVal;
+            }
+        });
+
+        return prev;
+    }, {});
+};
+
+class Stepper {
+
+    defaultSettings() {
+
+        return {
+            "container": "stepperContainer",
+            "customization": {
+                "container": {
+                    "classes": "container container-fluid my-4"
+                },
+                "steps": {
+                    "id": "stepsContainer",
+                    "classes": "my-2",
+                    "step": {
+                        "id": "stepperStep",
+                        "classes": "container border-1"
+                    }
+                },
+                "stepper": {
+                    "id": "stepperStepper",
+                    "show": true
+                },
+                "controls": {
+                    "id": "stepperControls",
+                    "classes": "btn-group d-flex mb-3",
+                    "nextBtn": {
+                        "id": "stepperNext",
+                        "classes": "btn text-center w-100 btn-outline-secondary",
+                        "label": "Next",
+                        "icon": "<i class='fas fa-arrow-alt-circle-right'></i>"
+                    },
+                    "backBtn": {
+                        "id": "stepperBack",
+                        "classes": "btn text-center w-100 btn-outline-secondary",
+                        "label": "Back",
+                        "icon": "<i class='fas fa-arrow-alt-circle-left'></i>"
+                    }
+                },
+                "loader": {
+                    "label": "Processing..."
+                }
+            },
+            "data": {},
+            "steps": [
+            ]
+        }
+    };
+
+    constructor(data) {
+        this.settings = mergeDeep(this.defaultSettings(), data);
+        this.controls = "";
+        this.stepper = "";
+        this.steps = "";
+        this.currentStep = 0;
+        this.totalSteps = (this.settings.steps).length;
+
+        this.checkSteps(this.settings.steps);
+
+        // Building layout
+        this.buildSteps();
+        this.buildControls();
+        this.buildStepper();
+
+    };
+
+    checkSteps(steps) {
+
+        for(var i = 0; i < steps.length; i++) {
+
+            var a = steps[i];
+
+            if(!a.hasOwnProperty("onNext") || typeof a.onNext !== "function") {
+
+                throw new Error("Step " + (i + 1) + " is missing the required onNext function or isn't a function.");
+                return;
+
+            }
+
+            if(!a.hasOwnProperty("onBack") || typeof a.onBack !== "function") {
+
+                throw new Error("Step " + (i + 1) + " is missing the required onBack function or isn't a function.");
+                return;
+
+            }
+
+        }
+
+    };
+
+    buildControls() {
+
+        // start Container
+        this.controls += "<div id='" + this.settings.customization.controls.id + "' class='" + this.settings.customization.controls.classes + "'>";
+
+        //Insert back button
+        this.controls += "<button id='" + this.settings.customization.controls.backBtn.id + "' class='" + this.settings.customization.controls.backBtn.classes + " disabled' disabled='disabled'>" +
+            this.settings.customization.controls.backBtn.label + "<br/>" + this.settings.customization.controls.backBtn.icon + "</button>";
+
+        //Insert next button
+        this.controls += "<button id='" + this.settings.customization.controls.nextBtn.id + "' class='" + this.settings.customization.controls.nextBtn.classes + "'>" +
+            this.settings.customization.controls.nextBtn.label + "<br/>" + this.settings.customization.controls.nextBtn.icon + "</button>";
+
+        // Close container
+        this.controls += "</div>";
+
+    };
+
+    buildStepper() {
+
+        if(!this.settings.customization.stepper.show) {
+
+            return;
+
+        }
+
+        // start container
+        this.stepper = "<div id='stepperProgress' class='card card-secondary hidden-print mb-3'>";
+        this.stepper += "<div class='card-body'>";
+        this.stepper += "<div class='row'></div>";
+        this.stepper += "<div class='row bs-wizard hidden-xs' style='border-bottom:0;'>";
+
+        //Build an item for each step
+        let colWidth = (12 / (Math.ceil(this.settings.steps.length)));
+
+        for(var i = 0; i < this.settings.steps.length; i++) {
+
+            var active;
+
+            if(i < 1) { active = " active"} else { active = "disabled"}
+
+            this.stepper += "<div id='stepperProgress-step" + (i + 1) + "' class='col col-xs-" + colWidth + " bs-wizard-step " + active + "'>";
+            this.stepper += "<div class='text-center bs-wizard-stepnum'>Step " + (i + 1) + "</div>";
+            this.stepper += "<div class='progress'>";
+                this.stepper += "<div class='progress-bar'></div>";
+            this.stepper += "</div>";
+            this.stepper += "<div class='bs-wizard-dot'></div>";
+            this.stepper += "<div class='bs-wizard-info text-center'>" + this.settings.steps[i].step_name + "</div>";
+            this.stepper += "</div>";
+
+            }
+
+        // End container
+        this.stepper += "</div></div></div>";
+
+    };
+
+    buildSteps() {
+
+        this.steps = "<div id='" + this.settings.customization.steps.id + "' class='" + this.settings.customization.steps.classes + "'>";
+
+        for(var i = 0; i < (this.settings.steps).length; i++) {
+
+            var display;
+
+            if(i >= 1) { display = "style='display:none;'";} else { display = "";}
+            this.steps += "<div id='" + this.settings.customization.steps.step.id + (i + 1) + "' class='" + this.settings.customization.steps.step.classes + "' " + display + ">" + this.settings.steps[ i].step_name + "</div>";
+        }
+
+        // Add the loader block
+        this.steps += "<div id='" + this.settings.customization.steps.step.id + "-loader' style='display:none;' class='text-center my-5'>";
+
+        this.steps += "<div class='spinner-grow text-secondary' style='width: 4rem; height: 4rem;' role='status'>";
+        this.steps += "<span class='sr-only'>" + this.settings.customization.loader.label + "</span>";
+        this.steps += "</div><h5 class='mt-5'>" + this.settings.customization.loader.label + "</h5>";
+
+        this.steps += "</div></div>";
+    };
+
+    load() {
+
+        let p = "#" + this.settings.container;
+
+        if(this.settings.customization.stepper.show) {
+
+            $(p).prepend(this.stepper);
+
+        }
+
+        $(p).append(this.steps);
+        $(p).append(this.controls);
+        this.startNavigationHandler();
+        this.settings.steps[this.currentStep].onLoad("#" + this.settings.customization.steps.step.id + (this.currentStep + 1), this.currentStep);
+    };
+
+    startNavigationHandler() {
+
+
+        // Next
+        $("#" + this.settings.customization.controls.nextBtn.id).on("click", function(){
+
+            var a = stepper.settings.steps[stepper.currentStep].onNext();
+
+            if(a === false) {
+
+                return;
+
+            } else {
+
+                stepper.settings.steps[stepper.currentStep].onLoad("#" + stepper.settings.customization.steps.step.id + stepper.currentStep, stepper.currentStep);
+
+                stepper.showStep((stepper.currentStep + 1));
+                stepper.updateNavigationStatus();
+
+            }
+
+        });
+
+        // Back
+        $("#" + this.settings.customization.controls.backBtn.id).on("click", function(){
+
+            if(stepper.settings.steps[stepper.currentStep].onBack() === false) {
+
+                return;
+
+            } else {
+
+                stepper.showStep((stepper.currentStep - 1));
+                stepper.updateNavigationStatus();
+
+
+            }
+
+        });
+
+    };
+
+    updateNavigationStatus() {
+
+        // Nav next btn
+        if((stepper.currentStep + 1) >= stepper.totalSteps){
+
+            stepper.btnStatus("navNext", false);
+
+        } else {
+
+            stepper.btnStatus("navNext", true);
+
+
+        }
+
+        // Nav back btn
+        if(stepper.currentStep == 0){
+
+            stepper.btnStatus("navBack", false);
+
+        } else {
+
+            stepper.btnStatus("navBack", true);
+
+
+        }
+
+        this.updateStepper(stepper.currentStep);
+
+    }
+
+    updateStepper(currentStep) {
+
+        if(!this.settings.customization.stepper.show){
+
+            return;
+
+        }
+
+        for(var i = 0; i < this.settings.steps.length; i++) {
+            this.removeStepperStatus((i + 1));
+
+            if(i < currentStep) {
+
+                $("#stepperProgress-step" + (i + 1)).addClass("complete");
+
+            } else if(i == currentStep) {
+
+                $("#stepperProgress-step" + (i + 1)).addClass("active");
+
+            } else if(i > currentStep) {
+
+                $("#stepperProgress-step" + (i + 1)).addClass("disabled");
+
+            } else {
+
+
+            }
+
+        }
+
+    }
+
+    removeStepperStatus(step) {
+
+        if($("#stepperProgress-step" + step).hasClass("active")) {
+
+            $("#stepperProgress-step" + step).removeClass("active");
+
+        } else if($("#stepperProgress-step" + step).hasClass("complete")) {
+
+            $("#stepperProgress-step" + step).removeClass("complete");
+
+        } else if($("#stepperProgress-step" + step).hasClass("disabled")) {
+
+            $("#stepperProgress-step" + step).removeClass("disabled");
+
+        }
+
+    }
+
+    updateStepContents(id, html) {
+
+        $(id).html(html);
+
+    }
+
+    btnStatus(btn, status) {
+
+        switch(btn) {
+
+            case "navBack":
+                if(status === false) {
+                    $("#" + this.settings.customization.controls.backBtn.id).addClass("disabled").attr("disabled", "disabled");
+                } else {
+                    $("#" + this.settings.customization.controls.backBtn.id).removeClass("disabled").removeAttr("disabled");
+                }
+                break;
+
+            case "navNext":
+                if(status === false) {
+                    $("#" + this.settings.customization.controls.nextBtn.id).addClass("disabled").attr("disabled", "disabled");
+                } else {
+                    $("#" + this.settings.customization.controls.nextBtn.id).removeClass("disabled").removeAttr("disabled");
+                }
+
+                break;
+
+            default:
+                return false;
+                break;
+
+        }
+
+        return true;
+
+    }
+
+    showStep(step) {
+
+        if(this.settings.steps[step] === false) {
+
+            alert("An error occurred.");
+
+        } else {
+            $("#" + this.settings.customization.steps.step.id + (this.currentStep + 1)).hide();
+            $("#" + this.settings.customization.steps.step.id + (step + 1)).show();
+
+            this.currentStep = step;
+
+        }
+
+    }
+
+    showLoader(status) {
+
+        if(status) {
+
+            $("#" + this.settings.customization.steps.step.id + (this.currentStep + 1)).hide();
+
+            $("#" + this.settings.customization.steps.step.id + "-loader").show();
+            this.btnStatus("navBack", false);
+            this.btnStatus("navNext", false);
+
+        } else {
+
+            $("#" + this.settings.customization.steps.step.id + "-loader").hide();
+            this.showStep(this.currentStep);
+            this.updateNavigationStatus();
+
+        }
+
+    }
+
+    test() {
+
+        this.buildSteps();
+
+        return this.steps;
+
+    };
+
+}
